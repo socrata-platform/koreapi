7c0710f69425d007409ab0ee4693c8cf9ed5728a
Michael Hotan <michael.hotan@socrata.com>
2015-01-30 09:55:44 -0800

    Fixed Broken GET domains request
    Stupid missing '/'

--- lib/domains.rb
+++ lib/domains.rb
@@ -8,11 +8,10 @@ class Domains
 
   def get_domains
 
-    address_and_port = CORE_ADDRESS.split(':')
-    service = Net::HTTP.new(address_and_port[0], address_and_port[1].to_i)
-    url = 'domains?method=all'
+    addr_port = CORE_ADDRESS.split(":")
+    service = Net::HTTP.new(addr_port[0], addr_port[1])
+    url = '/domains?method=all'
     request = Net::HTTP::Get.new(url)
-    # result = service.request(request)
     result = service.request(request)
     domain_data = JSON.parse(result.body)
     domains = {}
